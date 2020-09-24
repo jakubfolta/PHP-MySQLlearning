da5aa114cb8f966756a2967452a98837d3809a60
Jakub Folta <jakubfolta@yahoo.co.uk>
2020-09-24 09:52:38 +0100

    PHP crash course

--- processorder.php
+++ processorder.php
@@ -8,30 +8,32 @@
     <h2>Order Results</h2>
     <?php
       echo "<p>Order processed at ".date('H:i, jS F Y')."</p>";
-      // create short variable names
+      # create short variable names
       $tireqty = $_POST['tireqty'];
       $oilqty = $_POST['oilqty'];
       $sparkqty = $_POST['sparkqty'];
 
+      $totalqty = 0;
+      $totalqty = $tireqty + $oilqty + $sparkqty;
+      echo "<p>Items ordered: ".$totalqty."<br />";
+      $totalamount = 0.00;
       define('TIREPRICE', 100);
       define('OILPRICE', 10);
       define('SPARKPRICE', 4);
+      $totalamount = $tireqty * TIREPRICE
+                  + $oilqty * OILPRICE
+                  + $sparkqty * SPARKPRICE;
+      echo "Subtotal: $".number_format($totalamount,2)."<br />";
+      $taxrate = 0.10;  // local sales tax is 10%
+      $totalamount = $totalamount * (1 + $taxrate);
+      echo "Total including tax: $".number_format($totalamount,2)."</p>";
 
-      echo 'Tireprice '.TIREPRICE.'<br />';
-      echo 'Oilprice '.OILPRICE.'<br />';
-      echo 'Sparkprice '.SPARKPRICE.'<br />';
-
-      echo '<p>Your order is as follows: </p>';
-      echo $tireqty. ' Tires<br />';
-      echo $oilqty. ' bottles of oil<br />';
-      echo $sparkqty. ' spark plugs<br />';
-
-      // Using ternary operator
+      # Using ternary operator
 
       $grade=30;
       echo ($grade >= 40 ? '<p style="color:green"> Your score is ' .$grade. '. You passed the examination!! </p>' : '<p style="color:red"> Your score is ' .$grade. '. You failed the examination!! </p>')
 
-      // Type operator
+      # Type operator
 
       class sampleClass{};
       $myObject = new sampleClass();
